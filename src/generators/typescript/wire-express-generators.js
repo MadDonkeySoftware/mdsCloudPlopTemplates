2a1c2f0cf810083bbcd8db1bfdf56c6706003d83
Frito <fritogotlayed@gmail.com>
2022-08-15 08:45:16 -0500

    adding generic ts file template

--- src/generators/typescript/wire-express-generators.js
+++ src/generators/typescript/wire-express-generators.js
@@ -1,5 +1,6 @@
 import { controllerGenerator } from './express/controller';
 import { middlewareGenerator } from './express/middleware';
+import { generateBarrelFile } from '../../utils/generate-barrel-file';
 
 export function wireExpressGenerators(
   /** @type NodePlopAPI} */
@@ -7,4 +8,37 @@ export function wireExpressGenerators(
 ) {
   controllerGenerator(plop);
   middlewareGenerator(plop);
+
+  plop.setGenerator('ts-stub-with-barrel-file', {
+    description: 'generic file stub with barrel file and test (TypeScript)',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'file name',
+      },
+      {
+        type: 'input',
+        name: 'path',
+        message: 'relative file path',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '{{path}}/{{name}}.ts',
+        templateFile: 'templates/typescript/general-file.hbs',
+      },
+      {
+        type: 'add',
+        path: '{{path}}/__test__/{{name}}.test.ts',
+        templateFile: 'templates/typescript/general-file.test.hbs',
+      },
+      {
+        type: 'modify',
+        path: '{{path}}/index.ts',
+        transform: (body, data) => generateBarrelFile(body, data),
+      },
+    ],
+  });
 }
